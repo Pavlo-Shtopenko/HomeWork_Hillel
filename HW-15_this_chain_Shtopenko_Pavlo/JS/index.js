5fd6a5d6a02b06d5260667ee79d295b33c7f1486
Pavlo Shtopenko <shtopenko@gmail.com>
2023-05-15 22:07:23 +0300

    added HomeWork 15 ThisChain

---
+++ HW-15_this_chain_Shtopenko_Pavlo/JS/index.js
@@ -0,0 +1,25 @@
+let ladder = {
+  step: 0,
+  up: function () {
+      this.step++;
+      return this;
+  },
+  down: function () {
+      this.step--;
+      return this;
+  },
+  showStep: function () { // показывает текущую ступеньку
+      alert(this.step);
+  }
+};
+
+// ladder.up();
+// ladder.up();
+// ladder.up();
+// ladder.down();
+// ladder.showStep();
+// ladder.up().up().down().showStep();
+// ladder.up().up().showStep();
+// ladder.down();
+// ladder.showStep();
+// ladder.showStep();
